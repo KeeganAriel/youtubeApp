ed7da182bd6ca1f9459924f4dfd08e2d0e24e3aa
KeeganAriel <keegan.teinowitz@gmail.com>
2016-04-12 15:50:35 -0700

    js:add vars and .sub fun

---
+++ JS/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+	
+	$('#userSearch').submit(function(event) {
+		event.preventDefault();
+		var searchPhrase = $('#searchInput').val();
+	
+		$.getJSON('https://www.googleapis.com/youtube/v3/search', 
+			{key: "AIzaSyAitXKVR92IKLca15azobSZz7L9bL0Pt44", 
+			part: "snippet",
+			q: searchPhrase,
+			}, function(data) {
+				console.log(data);
+				displayResults(data);
+			});
+
+	});
+
+	var displayResults = function(resluts) {
+		var html = "";
+		$.each(resluts.items, function(index, value) {
+			html += '<img src="' + value.snippet.thumbnails.default.url + '">';
+		});
+		$('#searchResults').html(html);
+	};
+
+
+});
