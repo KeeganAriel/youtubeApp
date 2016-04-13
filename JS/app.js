8b55c0e04e14458dbedeac67bae6b6953c4ef134
KeeganAriel <keegan.teinowitz@gmail.com>
2016-04-12 17:57:19 -0700

    add videoID to url/ add link to video

--- JS/app.js
+++ JS/app.js
@@ -8,6 +8,13 @@ $(document).ready(function () {
 			{key: "AIzaSyAitXKVR92IKLca15azobSZz7L9bL0Pt44", 
 			part: "snippet",
 			q: searchPhrase,
+
+
+
+//  "https://www.googleapis.com/youtube/v3/videos?id=" +videoID+ "&key=" + 
+// YOUR KEY HERE + 
+// "&fields=items(id,snippet(channelId,title,categoryId),statistics)&part=snippet,statistics";
+
 			}, function(data) {
 				console.log(data);
 				displayResults(data);
@@ -18,7 +25,11 @@ $(document).ready(function () {
 	var displayResults = function(resluts) {
 		var html = "";
 		$.each(resluts.items, function(index, value) {
-			html += '<img src="' + value.snippet.thumbnails.default.url + '">';
+			html += '<p class="videoTitle">' + value.snippet.title + 
+			'</p><a href="https://www.youtube.com/watch?v=' + 
+			value.id.videoId + '"><img class="thumbnails" src="' + 
+			value.snippet.thumbnails.high.url + '"/></a>';
+			// html += '<img src="' + value.snippet.thumbnails.default.url + '">';
 		});
 		$('#searchResults').html(html);
 	};
