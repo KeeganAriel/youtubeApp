ad4fb94d6c94c0f2626f5cb4173954e4938a0159
KeeganAriel <keegan.teinowitz@gmail.com>
2016-04-13 18:40:14 -0700

    clean up

--- JS/app.js
+++ JS/app.js
@@ -8,13 +8,6 @@ $(document).ready(function () {
 			{key: "AIzaSyAitXKVR92IKLca15azobSZz7L9bL0Pt44", 
 			part: "snippet",
 			q: searchPhrase,
-
-
-
-//  "https://www.googleapis.com/youtube/v3/videos?id=" +videoID+ "&key=" + 
-// YOUR KEY HERE + 
-// "&fields=items(id,snippet(channelId,title,categoryId),statistics)&part=snippet,statistics";
-
 			}, function(data) {
 				console.log(data);
 				displayResults(data);
@@ -26,9 +19,9 @@ $(document).ready(function () {
 		var html = "";
 		$.each(resluts.items, function(index, value) {
 			html += '<p class="videoTitle">' + value.snippet.title + 
-			'</p><a href="https://www.youtube.com/watch?v=' + 
-			value.id.videoId + '"><img class="thumbnails" src="' + 
-			value.snippet.thumbnails.high.url + '"/></a>';
+				'</p><a href="https://www.youtube.com/watch?v=' + 
+				value.id.videoId + '"><img class="thumbnails" src="' + 
+				value.snippet.thumbnails.high.url + '"/></a>';
 			// html += '<img src="' + value.snippet.thumbnails.default.url + '">';
 		});
 		$('#searchResults').html(html);
