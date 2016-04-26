d300bee8d74ea4ec968a53b4b144e91c85568650
KeeganAriel <keegan.teinowitz@gmail.com>
2016-04-26 12:14:44 -0700

    clean up code

--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@ $(document).ready(function () {
 	$('#userSearch').submit(function(event) {
 		event.preventDefault();
 		var searchPhrase = $('#searchInput').val();
-	
+		
 		$.getJSON('https://www.googleapis.com/youtube/v3/search', 
 			{key: "AIzaSyAitXKVR92IKLca15azobSZz7L9bL0Pt44", 
 			part: "snippet",
 			q: searchPhrase,
-			}, function(data) {
-				console.log(data);
-				displayResults(data);
-			});
+		}, function(data) {
+			console.log(data);
+			displayResults(data);
+		});
 		$('#searchInput').val('');
 	});
 
